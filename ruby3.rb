6457c4c2b5cec8f566b83be15f0d00ee23578a22
sengoku4902 <sasazaki4902@gmail.com>
2020-12-11 19:06:50 +0900

    1211

---
+++ ruby3.rb
@@ -0,0 +1,12 @@
+puts "入力せよ"
+text = gets
+puts text
+puts "入力せよ"
+text = gets
+puts "入力したのは" + text
+puts "価格を入力せよ"
+kakaku = gets
+puts kakaku * 0.7
+puts "価格を入力せよ"
+kakaku = gets.to_i
+puts kakaku * 0.7
