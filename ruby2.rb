148adda81a94a1dcde4a6338103c5513c8c57e8e
sengoku4902 <sasazaki4902@gmail.com>
2020-12-11 18:58:56 +0900

    1211

--- ruby2.rb
+++ ruby2.rb
@@ -2,3 +2,12 @@ puts 2 + 0.5
 puts 2 + 10 + 5
 puts 2 + 10 * 5
 puts (2 + 10) * 5
+puts 5 / (4 * (1 - 0.2))
+text = "ハロー！"
+puts text
+kakaku = 100
+urine = kakaku * 0.7
+puts urine
+kakaku = 150
+urine = kakaku * 0.7
+puts urine
