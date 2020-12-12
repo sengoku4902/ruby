5f665867d54e1b5744706bad89506be015b0b6c7
sengoku4902 <sasazaki4902@gmail.com>
2020-12-12 22:02:15 +0900

    1212

--- ruby7.rb
+++ ruby7.rb
@@ -1,5 +1,17 @@
 shikin = 30000
 while shikin >= 0
     puts shikin
-    shikin = shikin - 5080 
+    shikin -= 5080
+end
+
+wdays =["月", "火", "水", "木", "金"]
+puts wdays[1]
+
+wdays = ["月", "火", "水", "木", "金"]
+wdays.delete("金")
+puts wdays
+
+wdays = ["月", "火", "水", "木", "金"]
+wdays.each do |wday|
+    puts wday + "曜日"
 end
