28aa061f2df02e17c9c0f07032a68c20771936b6
sengoku4902 <sasazaki4902@gmail.com>
2020-12-14 00:01:42 +0900

    1213

--- ruby11.rb
+++ ruby11.rb
@@ -19,3 +19,13 @@ def add_charge(bill)
 end
 
 puts add_charge(40000)
+
+def add_charge(bill)
+    if bill < 0
+        return "負の数は計算できない"
+    end
+    return (bill * 1.07).to_i
+end
+
+puts add_charge(40000)
+puts add_charge(-1111)
