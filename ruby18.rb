b8e7801e193e6d8e4b7b7c3440677657ef0bbceb
sengoku4902 <sasazaki4902@gmail.com>
2020-12-27 23:31:43 +0900

    1227

---
+++ ruby18.rb
@@ -0,0 +1,52 @@
+n = 11
+if n > 10
+    puts "10より大きい"
+else
+    puts "10より小さい"
+end
+
+country = "italy"
+if country == "japan"
+    puts "こんにちは"
+elsif country == "us"
+    puts "Hello"
+elsif country == "italy"
+    puts "ciao"
+else
+    puts "???"
+end
+
+country = "italy"
+if country == "japan"
+    "こんにちは"
+elsif country == "us"
+    "Hello"
+elsif country == "italy"
+    "ciao"
+else
+    "???"
+end
+
+country = "italy"
+
+greeting = 
+if country == "japan"
+elsif country == "us"
+    "Hello"
+elsif country == "italy"
+    "ciao"
+else
+    "???"
+end
+
+puts country
+
+
+
+point = 7
+day = 1
+if day == 1
+    point *=5
+end
+
+puts point
